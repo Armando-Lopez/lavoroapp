f27daa6d02954df983f8642b40930d5f0c512415
Diego Lopez <lopexdiego5@gmail.com>
2020-06-19 11:56:02 -0500

    set up

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "materialize-css/dist/css/materialize.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <>
+      <h1>Project under construction</h1>
+      <p>lorem</p>
+    </>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
