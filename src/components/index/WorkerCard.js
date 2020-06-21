b213a4c8fdefe8e2a401e814559ea76eb6a456b4
Diego Lopez <lopexdiego5@gmail.com>
2020-06-20 22:10:49 -0500

    loader added

--- src/components/index/WorkerCard.js
+++ src/components/index/WorkerCard.js
@@ -7,11 +7,7 @@ const WorkerCard = ({ id, first_name, last_name, photo, services }) => {
       <Link to={`workerprofile/${id}`}>
         <img src={photo} alt={first_name} className="circle" />
         <span className="title">{first_name + " " + last_name}</span>
-        <p>
-          {services.map((service) => {
-            return service + ". ";
-          })}
-        </p>
+        <p>{services.map((service) => service + ". ")}</p>
       </Link>
     </li>
   );
--- src/components/index/WorkersSection.js
+++ src/components/index/WorkersSection.js
@@ -1,35 +1,50 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import WorkerCard from "./WorkerCard";
+import Loader from "../Loader";
 
 const WorkersSection = () => {
   const [WorkersList, FillWorkersList] = useState([]);
+  const [loaded, setLoaded] = useState(false);
 
   const GetWorkers = async () => {
-    const response = await axios.get("./utils/MOCK_WORKERS_LIST.json");
-    FillWorkersList(response.data);
+    try {
+      const response = await axios.get("./utils/MOCK_WORKERS_LIST.json");
+      FillWorkersList(response.data);
+      setLoaded(true);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
     GetWorkers();
   }, []);
 
-  return (
-    <ul className="collection workers-list">
-      {WorkersList.map((worker, index) => {
-        return (
-          <WorkerCard
-            key={worker.id}
-            id={worker.id}
-            first_name={worker.first_name}
-            last_name={worker.last_name}
-            photo={worker.photo}
-            services={worker.services}
-          />
-        );
-      })}
-    </ul>
-  );
+  if (WorkersList && loaded) {
+    return (
+      <ul className="collection workers-list">
+        {WorkersList.map((worker, index) => {
+          return (
+            <WorkerCard
+              key={index}
+              id={worker.id}
+              first_name={worker.first_name}
+              last_name={worker.last_name}
+              photo={worker.photo}
+              services={worker.services}
+            />
+          );
+        })}
+      </ul>
+    );
+  } else {
+    return (
+      <section className=" section center-align">
+        <Loader />
+      </section>
+    );
+  }
 };
 
 export default WorkersSection;
